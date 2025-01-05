149b0dfd63d888803d91fec2a6d5cc66d56874e8
THANEES01 <s.thanees01@gmail.com>
2025-01-05 11:52:06 +0800

    Updated vercel

--- db.js
+++ db.js
@@ -2,21 +2,43 @@
 import pkg from 'pg';
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'kalakshetra_db',
+//     password: '#As011416#',
+//     port: 5432,
+// });
+
+// // Test database connection
+// pool.query('SELECT NOW()', (err, res) => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//     } else {
+//         console.log('Successfully connected to PostgreSQL database');
+//     }
+// });
+
+// export default pool;
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'kalakshetra_db',
-    password: '#As011416#',
-    port: 5432,
-});
-
-// Test database connection
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-    } else {
-        console.log('Successfully connected to PostgreSQL database');
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
     }
-});
-
-export default pool;
+  });
+  
+  // Test connection
+  const testConnection = async () => {
+    try {
+      const client = await pool.connect();
+      console.log('Database connected successfully.');
+      client.release();
+    } catch (err) {
+      console.error('Database connection error:', err);
+    }
+  };
+  
+  testConnection();
+  
+  module.exports = pool;
